92f856ab3e34996d443164e2ad8651f06dfe5c5c
Nils Drees <Nylox.mx@gmail.com>
2021-01-29 19:26:00 +0100

    Revert "FPS"
    This reverts commit a41834a368cc8d127f1c92c4ba7211384461bc44.

--- DirectXEngine/Graphics/Graphics.h
+++ DirectXEngine/Graphics/Graphics.h
@@ -1,17 +1,27 @@
 #pragma once
+#include <SpriteBatch.h>
+#include <SpriteFont.h>
 #include "AdapterReader.h"
 #include "Shaders.h"
 #include "Vertex.h"
+#include <WICTextureLoader.h>
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "ConstantBufferTypes.h"
+#include "ConstantBuffer.h"
+#include "Camera.h"
+#include "../Timer.h"
 
 class Graphics
 {
 public:
 	bool Initialize(HWND hwnd, int width, int height);
 	void RenderFrame();
+	Camera camera;
 private:
-	bool InitializeDirectX(HWND hwnd, int width, int height);
+	bool InitializeDirectX(HWND hwnd);
 	bool InitializeShaders();
-	bool InitializeSzene();
+	bool InitializeScene();
 
 	Microsoft::WRL::ComPtr<ID3D11Device> device;
 	Microsoft::WRL::ComPtr<ID3D11DeviceContext> deviceContext;
@@ -21,6 +31,24 @@ private:
 	VertexShader vertexshader;
 	PixelShader pixelshader;
 	
-	Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer;
+	 
+	VertexBuffer<Vertex> vertexBuffer;
+	IndexBuffer indicesBuffer;
+	ConstantBuffer<CB_VS_vertexshader> constantBuffer;
+
+	Microsoft::WRL::ComPtr<ID3D11DepthStencilView> depthStencilView;
+	Microsoft::WRL::ComPtr<ID3D11Texture2D> depthStencilBuffer;
+	Microsoft::WRL::ComPtr<ID3D11DepthStencilState> depthStencilState;
+
 	Microsoft::WRL::ComPtr<ID3D11RasterizerState> rasterizerState;
+
+	std::unique_ptr<DirectX::SpriteBatch> spriteBatch;
+	std::unique_ptr<DirectX::SpriteFont> spriteFont;
+
+	Microsoft::WRL::ComPtr<ID3D11SamplerState> samplerState;
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> myTexture;
+	int windowWidth = 0;
+	int windowHeight = 0;
+
+	Timer fpsTimer;
 };
